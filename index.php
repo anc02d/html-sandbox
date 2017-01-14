1b1e616c4caaac8bfc73a33f44b41619ca8561ba
Noel Cothren <noelcothren@gmail.com>
2017-01-14 15:16:02 -0700

    playing with hover and transitions

--- index.php
+++ index.php
@@ -3,22 +3,16 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>HTML Sandbox</title>
+		<link rel="stylesheet" href="style.css" type="text/css">
 	</head>
 	<body>
 		<header style="background-color: aquamarine"><h1>Header One for My Page</h1></header>
-		<section style="background-color: darkcyan">
 			<h2>A Heading for This First Section</h2>
-			<p>This is a paragraph with my page content.</p>
-				<aside>This is an aside, tangentally related to this paragraph content.</aside>
-			<p>And even more page content for this current paragraph, first section.</p>
-		</section>
-		<section style="background-color: darkslateblue">
-			<h2 style="color: azure">Another Header, Another Section</h2>
-			<p style="color: azure">Aaaaand, another paragraph.</p>
-		</section>
-		<h2>The Third Heading Intros My Table</h2>
-		<img src="images/highfive.jpg" alt="Cat and Dog High-Fiveing">
-		<table>
+		<div class="showFirst">
+			<span class="showme">I develop websites.</span>
+			<div class="ok">Hi! I'm Noel</div>
+		</div>
+			<table id="box">
 			<tr>
 				<th>Cats</th>
 				<th>Dogs</th>
